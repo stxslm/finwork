231a92b9386755d7ed278e423cc26eed9b37241c
Bruce Kevin <144119868+stxslm@users.noreply.github.com>
2025-06-16 19:51:15 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,29 @@
+window.onload=function(){
+    var audio=document.getElementsByTagName('audio')[0]
+
+
+
+    window.addEventListener('click',e=>{
+        audio.play()
+        
+    })
+}
+    // audio.addEventListener("canplaythrough", function(){
+
+    //     document.addEventListener('click',function(){
+    //        audio.play()
+    //        document.removeEventListener('click',arguments.callee)
+    //     })    
+      
+    //     document.addEventListener('mousewheel',function(){
+    //        audio.play()
+    //        document.removeEventListener('mousewheel',arguments.callee)
+    //     })
+          
+    //     document.addEventListener('touchstart',function(){
+    //       //增加一个touchstart交互事件，触屏后播放音乐
+    //       audio.play()
+    //       //播放音频后移除touchstart事件的当前匿名函数
+    //       document.removeEventListener('touchstart',arguments.callee)
+    //     })
+    //   })
